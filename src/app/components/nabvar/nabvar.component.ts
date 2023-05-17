f5cc109cd3db5169277e690d0215af16abd895c2
joseph517 <alvarovergara1993@hotmail.com>
2023-05-17 08:33:34 -0500

    create view home, nabvar and add logo

--- src/app/components/nabvar/nabvar.component.ts
+++ src/app/components/nabvar/nabvar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-nabvar',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./nabvar.component.css']
 })
 export class NabvarComponent {
+
+   constructor(private router: Router) {}
+
+  isHomeRoute(): boolean {
+    return this.router.url === '/';
+  }
+
+  isSignupRoute(): boolean {
+    return this.router.url === '/signup';
+  }
+
+  isLoginRoute(): boolean {
+    return this.router.url === '/login';
+  }
 
 }
